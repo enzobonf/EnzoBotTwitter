612c228f4e352054d1af04876a05e7683945e1fe
enzobonf <enzobonfx@gmail.com>
2020-04-14 03:57:01 -0300

    RT modificado apenas para tweets que contém em sí mesmos a palavra "Enzo"

--- index.js
+++ index.js
@@ -14,6 +14,28 @@ const bot = new twit(credentials);
 let tweetsRetweetados = [];
 let tweetsFavoritados = [];
 
+function wordCount(string, word) {
+
+    var length = typeof string === "string" && typeof word === "string" && word.length,
+        loop = length,
+        index = 0,
+        count = 0;
+
+    while (loop) {
+        index = string.indexOf(word, index);
+        if (index !== -1) {
+            count += 1;
+            index += length;
+        } else {
+            loop = false;
+        }
+    }
+
+    return count;
+}
+
+//console.log(wordCount('enzoenzo', 'enzo'));
+
 function search(q, lang, count){
 
     return new Promise((resolve, reject)=>{
@@ -24,18 +46,45 @@ function search(q, lang, count){
                 reject(err);
             }
             else{
-                let id, tweet = data.statuses[0];
-                if(tweet !== undefined) id = tweet.id_str;
+                let id, tweet = data.statuses[0], inResponseTo, text;
 
-                if(tweet && id && !tweet.retweeted_status && tweetsRetweetados.indexOf(id) === -1){
-                    resolve(tweet);
+                if(tweet !== undefined) {
+
+                    console.log('\n tweet:', tweet.text);
+
+                    id = tweet.id_str;
+
+                    inResponseTo = tweet.in_reply_to_screen_name;
+                    (inResponseTo !== null) ? inResponseTo = inResponseTo.toLowerCase() : '';
+
+                    //(!tweet.truncated) ? text = tweet.text : text = tweet.extended_tweet.full_text;
+                    text = tweet.text;
+                    text = text.toLowerCase();
+
+                    console.log('Quantas vezes Enzo aparece no tweet:', wordCount(text, 'enzo'));
+
+                    console.log(inResponseTo);
+
+                    //console.log(tweet.text.toLowerCase());
+                    console.log(text.indexOf('enzo'));
+
+
+                    if(tweet && id && !tweet.retweeted_status && tweetsRetweetados.indexOf(id) === -1 && text.indexOf('enzo') !== -1
+                    && (inResponseTo === null || wordCount(text.replace(inResponseTo), 'enzo') >= 1)){
+                        resolve(tweet);
+                    }
+                    else{
+                        console.log('\nTweet não contém "Enzo", já retweetado, com erro, ou é retweet, daqui a 20 segundos será procurado outro');
+                        setTimeout(() => {
+                            search(q, lang, count);
+                        }, 40000);     
+                    }
+
                 }
                 else{
-                    console.log('\nTweet já retweetado, com erro, ou é retweet, daqui a 20 segundos será procurado outro');
-                    setTimeout(() => {
-                        search(q, lang, count);
-                    }, 20000);     
+                    reject('ERRO');
                 }
+
             }
 
         });
@@ -46,56 +95,62 @@ function search(q, lang, count){
 
 function retweet(tweet){
 
-    const id = tweet.id_str;
+    return new Promise((resolve, reject)=>{
 
-    bot.post('statuses/retweet/:id', {id}, (err, data, response)=>{
-        if(!err){
-            tweetsRetweetados.push(id);  
-            console.log('\nRetweetado:', tweet.text);
-            console.log('Nº de tweets retweetados:', tweetsRetweetados.length);
-        }
-        else{
-            console.error('ERRO retweet:', err.message);
-        }
+        const id = tweet.id_str;
+
+        bot.post('statuses/retweet/:id', {id}, (err, data, response)=>{
+            if(!err){
+                tweetsRetweetados.push(id);  
+                resolve({text: tweet.text, id});
+            }
+            else{
+                reject('ERRO retweet:', err.message);
+            }
+        });
+
     });
+    
 
 }
 
-function favorite(tweet){
+/* function favorite(tweet){
 
-    const id = tweet.id_str;
+    return new Promise((resolve, reject)=>{
 
-    bot.post('favorites/create', {id}, (err, response)=>{
-        if(!err){
-            tweetsFavoritados.push(id);
-            console.log('Nº de tweets favoritados:', tweetsFavoritados.length);
-            console.log(tweet.text, id);
-        }
-        else{
-            console.log('ERRO fav:', err.message);
-        }
+        const id = tweet.id_str;
+
+        bot.post('favorites/create', {id}, (err, response)=>{
+            if(!err){
+                tweetsFavoritados.push(id);
+                resolve({text: tweet.text, id});
+            }
+            else{
+                reject('ERRO fav:', err.message);
+            }
+        });
+
     });
 
-}
+    
+
+} */
 
 
  setInterval(() => {
 
     try{
-        /*bot.get('search/tweets', { q: '"Enzo"', lang: 'pt', count: 1 }, function(err, data, response) {
-
-            let tweet = data.statuses[0];
-        
-            if(tweet && tweet.id_str && !tweet.retweeted_status){
-                retweet(tweet);
-                favorite(tweet);
-            }
-
-        });*/
 
         search('"Enzo"', 'pt', 1).then(tweet=>{
 
-            retweet(tweet);
+            retweet(tweet).then(response=>{
+
+                console.log('Nº de tweets retweetados:', tweetsRetweetados.length);
+                console.log('Retweetado:', response.text);
+
+            }).catch(err=>{
+                console.log(err);
+            });
 
         }).catch(err=>{
             console.log(err);
@@ -103,7 +158,7 @@ function favorite(tweet){
 
     }
     catch(e){
-        console.log('ERRO:', e);
+        console.log('ERRO no setInterval()', e);
     }
 
 }, 40000);
