ad6ef7380e2880c52ff1fa07e038abb4371f13e6
enzobonf <enzobonfx@gmail.com>
2020-04-16 04:44:54 -0300

    Update botFoz.js

--- botFoz.js
+++ botFoz.js
@@ -84,7 +84,7 @@ module.exports = (radius, count) => {
     
                     }).catch(err=>{
                         console.log('ERRO retweet:', err.message);
-                    });;
+                    });
 
                 });
                 
