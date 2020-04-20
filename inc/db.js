f9415eda27b3e4f8b94b2762577fa5c627a5e74a
enzobonf <enzobonfx@gmail.com>
2020-04-20 02:04:33 -0300

    Update db.js

--- inc/db.js
+++ inc/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/botsConfig?replicaSet=rs0', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/botsConfig', {useNewUrlParser: true, useUnifiedTopology: true});
 
 let db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'));
